7484921bc2e1d4cccf824920853d12c7ce5d3656
srb932 <luka.j08@yahoo.com>
2026-06-01 21:29:04 +0200

    Fix: Ajout du dossier src dans le content de tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/features/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+],
   theme: {
     extend: {
       colors: {
